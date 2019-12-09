3ce3aa9123079a5c1aa1f67220fef8a23d0a9e1b
Liviu Chiribuca <cliviu74@yahoo.com>
2019-12-09 21:30:28 +0000

    added zigbee2mqtt install script

---
+++ install-zigbee2mqtt-rpi-zero.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v10.0.0.sh | bash
+sudo apt-get install -y git make g++ gcc
+sudo git clone https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
+sudo chown -R pi:pi /opt/zigbee2mqtt
+cd /opt/zigbee2mqtt
+npm install

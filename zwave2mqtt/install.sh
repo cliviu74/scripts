4f3a943b946b704b6244d202456c8eedb218c712
Liviu Chiribuca <lchiribuca@poppulo.com>
2019-12-10 14:32:10 +0000

    add directory creation to install script

--- zwave2mqtt/install.sh
+++ zwave2mqtt/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo mkdir -p /opt/zwave2mqtt/store
 sudo wget -O /lib/systemd/system/zwave2mqtt.service https://raw.githubusercontent.com/cliviu74/scripts/master/zwave2mqtt/zwave2mqtt.service
 sudo systemctl daemon-reload
 sudo systemctl enable zwave2mqtt

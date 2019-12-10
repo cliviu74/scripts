104abfcb4e06e39c874f23b1c2ac075f53e7080d
Liviu Chiribuca <lchiribuca@poppulo.com>
2019-12-10 10:59:37 +0000

    Added sudo to priviledged commands

--- zigbee2mqtt/install.sh
+++ zigbee2mqtt/install.sh
@@ -5,8 +5,9 @@ sudo git clone https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
 sudo chown -R pi:pi /opt/zigbee2mqtt
 cd /opt/zigbee2mqtt
 npm install
-wget -O /lib/systemd/system/zigbee2mqtt.service https://raw.githubusercontent.com/cliviu74/scripts/master/zigbee2mqtt/zigbee2mqtt.service
-systemctl daemon-reload
-systemctl enable zigbee2mqtt
-systemctl start zigbee2mqtt
-wget -O /usr/bin/zigbee2mqtt-update.sh https://raw.githubusercontent.com/cliviu74/scripts/master/zigbee2mqtt/zigbee2mqtt-update.sh
+sudo wget -O /lib/systemd/system/zigbee2mqtt.service https://raw.githubusercontent.com/cliviu74/scripts/master/zigbee2mqtt/zigbee2mqtt.service
+sudo systemctl daemon-reload
+sudo systemctl enable zigbee2mqtt
+sudo systemctl start zigbee2mqtt
+sudo wget -O /usr/local/bin/zigbee2mqtt-update.sh https://raw.githubusercontent.com/cliviu74/scripts/master/zigbee2mqtt/zigbee2mqtt-update.sh
+sudo chmod +x /usr/local/bin/zigbee2mqtt-update.sh 

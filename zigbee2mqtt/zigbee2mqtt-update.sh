76dc1c37ff0624c8009efd9fe933c2a28bbc9552
cliviu74 <cliviu74@yahoo.com>
2020-05-20 17:46:39 +0100

    Changed zigbee2mqtt-update script
    Changes to suit the new deployment script for raspberry pi

--- zigbee2mqtt/zigbee2mqtt-update.sh
+++ zigbee2mqtt/zigbee2mqtt-update.sh
@@ -1,20 +1,21 @@
-#!/bin/bash
-# Stop zigbee2mqtt and go to directory
-sudo systemctl stop zigbee2mqtt
-cd /opt/zigbee2mqtt
 
-# Backup configuration
-cp -R data data-backup
+#!/bin/env bash
 
-# Update
-git checkout HEAD -- npm-shrinkwrap.json
-git pull
-rm -rf node_modules
-npm install
+Z2M_DEFAULT_VERSION='1.13.1'
+Z2M_VERSION=${1:-$Z2M_DEFAULT_VERSION}
 
-# Restore configuration
-cp -R data-backup/* data
-rm -rf data-backup
-
-# Start zigbee2mqtt
-sudo systemctl start zigbee2mqtt
+echo "Installing zigbee2mqtt-$Z2M_VERSION"
+echo "Cloning repo..."
+git clone https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt-$Z2M_VERSION/
+echo "Apply permissions..."
+sudo chown -R pi:pi /opt/zigbee2mqtt-$Z2M_VERSION/
+echo "npm install..."
+cd /opt/zigbee2mqtt-$Z2M_VERSION/
+sudo -u pi npm ci 
+echo "Copy data directory from current install"
+cp -ax /opt/zigbee2mqtt/data/* /opt/zigbee2mqtt-$Z2M_VERSION/data/
+echo "Starting Zigbee2Mqtt version $Z2M_VERSION"
+systemctl stop zigbee2mqtt
+rm /opt/zigbee2mqtt
+ln -s /opt/zigbee2mqtt-$Z2M_VERSION/ /opt/zigbee2mqtt
+systemctl start zigbee2mqtt

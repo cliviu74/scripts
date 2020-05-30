eea2a0e01391cb457d76df32cd98372563614fbe
cliviu74 <cliviu74@yahoo.com>
2020-05-30 21:13:49 +0100

    Update install.sh

--- freenas-console/install.sh
+++ freenas-console/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "Installing script to /usrl/local/bin/"
-cp freenas-console.sh /usr/local/bin
-ln -s /usr/local/bin/freenas-console.sh /usr/local/bin/freenas-console
+curl https://raw.githubusercontent.com/cliviu74/scripts/master/freenas-console/freenas-console.sh -o /usr/local/bin/freenas-console.sh
+#cp freenas-console.sh /usr/local/bin
 chmod +x /usr/local/bin/freenas-console.sh
+ln -s /usr/local/bin/freenas-console.sh /usr/local/bin/freenas-console

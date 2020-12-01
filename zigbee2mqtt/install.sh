afe060c09669a41a39391e019a431339fa3b4418
Liviu Chiribuca <Liviu Chiribuca>
2020-12-01 20:33:21 +0000

    Add exit on error to install script

--- zigbee2mqtt/install.sh
+++ zigbee2mqtt/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v10.0.0.sh | bash
 sudo apt-get install -y git make g++ gcc
 sudo git clone https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
